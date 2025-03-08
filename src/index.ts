2a8694eb059d88a1068efeed78fd92c72017fb14
Ox52 <harsh04241@gmail.com>
2025-03-09 00:46:47 +0530

    add files

--- src/index.ts
+++ src/index.ts
@@ -31,4 +31,4 @@ app.delete("/api/v1/content",(req,res)=>{
 app.post("api/v1/brain/share",(req,res)=>{})
 
 
-app.get("api/v1/brain/:shareLink",(req,res)=>{})
+app.get("api/v1/brain/:shareLink",(req,res)=>{})  
